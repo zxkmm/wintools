78037fccdd5b0c0fa1d0efebba0d059ef7537f7a
pathetic nihilist <zxkmm@hotmail.com>
2023-09-28 21:17:03 +0800

    full py

--- 全拼.cmd
+++ 全拼.cmd
@@ -1,8 +1,9 @@
 @echo off
 
 set Mainkey=HKCU\SOFTWARE\Microsoft\InputMethod\Settings\CHS
+
 for /f %%i in ('reg query %MainKey% /v "Enable Double Pinyin" ^| findstr /i "0x1"') do (set flg=%%i)
 
-    reg add %MainKey% /v "Enable Double Pinyin" /t REG_DWORD /d 0x0 /f
-    (echo switched to full pinyin
-    echo closing in 1s)|msg %username% /time:1
+reg add %MainKey% /v "Enable Double Pinyin" /t REG_DWORD /d 0x0 /f
+(echo switched to full pinyin
+echo closing in 1s)|msg %username% /time:1

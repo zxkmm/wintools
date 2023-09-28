40cfc3dd11669a0afe1c932500c5e60e330596f2
pathetic nihilist <zxkmm@hotmail.com>
2023-09-28 21:17:33 +0800

    textual

--- 双拼.cmd
+++ 双拼.cmd
@@ -1,9 +1,10 @@
 @echo off
 
 set Mainkey=HKCU\SOFTWARE\Microsoft\InputMethod\Settings\CHS
+
 for /f %%i in ('reg query %MainKey% /v "Enable Double Pinyin" ^| findstr /i "0x1"') do (set flg=%%i)
 
-    reg add %MainKey% /v "Enable Double Pinyin" /t REG_DWORD /d 0x1 /f
-    echo switched to double pinyin
-    (echo switched to double pinyin
-    echo closing in 1s)|msg %username% /time:1
+reg add %MainKey% /v "Enable Double Pinyin" /t REG_DWORD /d 0x1 /f
+echo switched to double pinyin
+(echo switched to double pinyin
+echo closing in 1s)|msg %username% /time:1
